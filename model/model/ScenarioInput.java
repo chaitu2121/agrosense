91b2e5010ace4b65fd1b2a1644792b9776bba625
chaitu2121 <chandrakantshitole6714@gmail.com>
2026-02-22 13:03:27 +0530

    Update ScenarioInput.java

--- model/model/ScenarioInput.java
+++ model/model/ScenarioInput.java
@@ -1,1 +1,20 @@
+package com.agrosense.model;
 
+public class ScenarioInput {
+
+    private double temperatureRise;   // in °C
+    private double rainfallChange;    // % increase or decrease
+
+    public ScenarioInput(double temperatureRise, double rainfallChange) {
+        this.temperatureRise = temperatureRise;
+        this.rainfallChange = rainfallChange;
+    }
+
+    public double getTemperatureRise() {
+        return temperatureRise;
+    }
+
+    public double getRainfallChange() {
+        return rainfallChange;
+    }
+}
